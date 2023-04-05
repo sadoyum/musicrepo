4a5e690ff3014f11e181b6002b76149d223a1a00
sadoyum <92607984+sadoyum@users.noreply.github.com>
2023-04-05 15:47:01 +0300

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -4,8 +4,9 @@ from pyrogram import filters
 
 from config import COMMAND_PREFIXES
 
-other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
-other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+other_filters = filters.group & ~ filters.edited_messages & ~ filters.via_bot & ~ filters.forwarded
+other_filters2 = filters.private & ~ filters.edited_messages & ~ filters.via_bot & ~ filters.forwarded
+
 
 
 def command(commands: Union[str, List[str]]):
